264761d77a0116161c831abeb700ddca0a2c06b9
Daniel Avrukin <davrukin@gmail.com>
2018-07-09 12:07:44 -0700

    added customized fast-list component, some test implementation

--- app/src/main/java/net/davrukin/jobapplicationtracker/ui/fragments/StartFragment.kt
+++ app/src/main/java/net/davrukin/jobapplicationtracker/ui/fragments/StartFragment.kt
@@ -10,9 +10,13 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.start_fragment.*
 import net.davrukin.jobapplicationtracker.R
 import net.davrukin.jobapplicationtracker.ui.viewmodels.StartViewModel
+import com.list.rados.fast_list.bind
+import com.list.rados.fast_list.update
+import net.davrukin.jobapplicationtracker.extensions.bind
 
 class StartFragment : Fragment() { // for some reason not working as NavHostFragment()
 
@@ -21,12 +25,47 @@ class StartFragment : Fragment() { // for some reason not working as NavHostFrag
     }
 
     private lateinit var viewModel: StartViewModel
+	private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 	    button_add_job_app.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.newJobApplicationFragment, savedInstanceState))
+		recyclerView = recycler_view
+	    setUpRecyclerView()
 
         return inflater.inflate(R.layout.start_fragment, container, false)
     }
+
+	/**
+	 * Using FastList
+	 *
+	 * Simple Single Layout List With No Layout
+	 */
+	private fun setUpRecyclerView() {
+		val list = listOf<String>("pizza", "hamburger", "hotdog", "antelope", "lion", "tiger")
+		recyclerView.bind(list)
+	}
+
+	/**
+	 * Using FastList
+	 *
+	 * Simple Single Layout List with Custom Layout
+	 */
+	private fun setUpRecyclerView2() {
+		val list = listOf<String>("pizza", "hamburger", "hotdog", "antelope", "lion", "tiger")
+		/*recyclerView.bind(list, ) {it : Item ->
+
+		}*/
+	}
+
+	/**
+	 * Using FastList
+	 *
+	 * Dynamic List with Multiple Layouts
+	 */
+	private fun setUpRecyclerView3() {
+		val list = listOf<String>("pizza", "hamburger", "hotdog", "antelope", "lion", "tiger")
+		//recyclerView.bind(list).map()
+	}
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
